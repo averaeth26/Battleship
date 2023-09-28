87bf71e5b61f3de102e6819a013d4407265beae0
Ethan Avera <averaeth26@7hills.org>
2023-09-28 12:33:11 -0400

    Created board and tester classes
    Created printBoard() method, which prints each element of the board in sequence.

---
+++ Board.java
@@ -0,0 +1,14 @@
+public class Board {
+    int numRows = 10;
+    int numCols = 10;
+    int[] ships = new int[5]; // {2, 3, 3, 4, 5}
+    int[][] grid = new int[numRows][numCols];
+    public void printBoard() {
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numCols; c++) {
+                System.out.print(grid[r][c] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        Board grid = new Board();
+        grid.printBoard();
+    }
+}
