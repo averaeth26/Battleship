1a6f85b24c09a9f21658414e6890c70818ab943c
Ethan Avera <averaeth26@7hills.org>
2023-09-28 13:04:49 -0400

    Nearly finished the setup function in Board.
    This function places ships according to user input.

--- Board.java
+++ Board.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Board {
+    Scanner scan = new Scanner(System.in);
     int numRows = 10;
     int numCols = 10;
-    int[] ships = new int[5]; // {2, 3, 3, 4, 5}
-    int[][] grid = new int[numRows][numCols];
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    int[] ships = {2, 3, 3, 4, 5};
+    int[][] game = new int[numRows][numCols];
+
+    public void setup() {
+        for(int ship = 0; ship < ships.length; ship++) {
+            System.out.print("Where would you like to place your " + ships[ship] + " tile long ship: ");
+            String tileNum = scan.nextLine();
+            System.out.println((int)(tileNum.charAt(1))-'1');
+            game[(int)(tileNum.toLowerCase().charAt(0)) - 'a'][(int)(tileNum.charAt(1))-'1'] = 1;
+        }
+    }
+
     public void printBoard() {
         for (int r = 0; r < numRows; r++) {
+            System.out.print((char)(r+'a') + " ");
             for (int c = 0; c < numCols; c++) {
-                System.out.print(grid[r][c] + " ");
+                System.out.print(game[r][c] + " ");
             }
             System.out.println("");
         }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
         Board grid = new Board();
         grid.printBoard();
+        grid.setup();
     }
 }
