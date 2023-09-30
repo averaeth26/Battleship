20387e847fb61b71a0e1778b24ce8e56f8117a5f
MrScorcher1 <eavera@theaveras.net>
2023-09-29 22:47:47 -0400

    Completed player placing ships in the setup method.
    Some of the code is a bit hard to read, and I plan on reformatting some parts of this later.
    Consider this a fist draft for now, but it is functional.

--- Board.java
+++ Board.java
@@ -10,18 +10,49 @@ public class Board {
 
     public void setup() {
         for(int ship = 0; ship < ships.length; ship++) {
-            System.out.print("Where would you like to place your " + ships[ship] + " tile long ship: ");
+            System.out.print("Where would you like to place one end of your " + ships[ship] + " tile long ship?: ");
             String tileNum = scan.nextLine();
-            System.out.println((int)(tileNum.charAt(1))-'1');
-            game[(int)(tileNum.toLowerCase().charAt(0)) - 'a'][(int)(tileNum.charAt(1))-'1'] = 1;
+            int startPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
+            int startPosCol = Integer.parseInt(tileNum.substring(1))-1;
+            while (startPosRow > 9 || startPosRow < 0 || startPosCol > 9 || startPosCol < 0 || game[startPosRow][startPosCol] != 0) {
+            System.out.println("Invalid placement! Try again: ");
+            tileNum = scan.nextLine();
+            startPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
+            startPosCol = Integer.parseInt(tileNum.substring(1))-1;
+            }
+            System.out.println("Where would you like the other end of your ship to be placed?: ");
+            tileNum = scan.nextLine();
+            int endPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
+            int endPosCol = Integer.parseInt(tileNum.substring(1))-1;
+            while ((Math.abs(startPosRow - endPosRow) + Math.abs(startPosCol - endPosCol) != ships[ship]-1)
+            || (startPosRow != endPosRow && startPosCol != endPosCol)
+            || (endPosRow > 9 || endPosRow < 0 || endPosCol > 9 || endPosCol < 0)
+            || game[endPosRow][endPosCol] != 0) {
+            System.out.println("Invalid placement! Try again: ");
+            tileNum = scan.nextLine();
+            endPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
+            endPosCol = Integer.parseInt(tileNum.substring(1))-1;
+            System.out.println(endPosCol);
+            }
+            for (int r = Math.min(startPosRow, endPosRow); r <= Math.max(startPosRow, endPosRow); r ++) {
+                for (int c = Math.min(startPosCol, endPosCol); c <= Math.max(startPosCol, endPosCol); c ++) {
+                    game[r][c] = 1;
+                }
+            }
+            printBoard();
         }
     }
 
     public void printBoard() {
+        System.out.print("   ");
+        for (int i = 0; i < numCols; i ++) {
+            System.out.print(i+1 + "  ");
+        }
+        System.out.println("");
         for (int r = 0; r < numRows; r++) {
-            System.out.print((char)(r+'a') + " ");
+            System.out.print((char)(r+'a') + "  ");
             for (int c = 0; c < numCols; c++) {
-                System.out.print(game[r][c] + " ");
+                System.out.print(game[r][c] + "  ");
             }
             System.out.println("");
         }
