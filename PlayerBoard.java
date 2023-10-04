f70e40f6bec3e2ccd0e4afd5fa9677bcbae0da7f
Ethan Avera <averaeth26@7hills.org>
2023-10-04 13:03:14 -0400

    Continued working on resetting logic.

--- PlayerBoard.java
+++ PlayerBoard.java
@@ -6,25 +6,48 @@ public class PlayerBoard {
     int numCols = 10;
     int[] ships = {2, 3, 3, 4, 5};
     int[][] game = new int[numRows][numCols];
+
+//     public boolean checkReset(String inputTile) {
+//             if (inputTile.toLowerCase().equals("reset")) {
+//                     return true;
+//                 }
+//             return false;
+// }
     public void userRegularSetup() {
         // Loops through each of the ships from shortest to longest (in the order of the ships array).
-        for(int ship = 0; ship < ships.length; ship++) {
+        int numShips = ships.length;
+        for(int ship = 0; ship < numShips; ship++) {
+
+            boolean reset = false;
             System.out.print("Where would you like to place one end of your " + ships[ship] + " tile long ship?: ");
             String tileNum = scan.nextLine();
+            if (tileNum.toLowerCase().equals("reset")) {
+                System.out.println(tileNum);
+                    reset = true;
+                }
+            
             int startPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
             int startPosCol = Integer.parseInt(tileNum.substring(1))-1;
-            boolean reset = false;
+
 
 
             // While loop checks to see if the start pos is a valid location on the grid
             while (startPosRow > 9 || startPosRow < 0 || startPosCol > 9 || startPosCol < 0 || game[startPosRow][startPosCol] != 0) {
-            System.out.println("Invalid placement! Try again: ");
-            tileNum = scan.nextLine();
-            startPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
-            startPosCol = Integer.parseInt(tileNum.substring(1))-1;
+                System.out.println("Invalid placement! Try again: ");
+                tileNum = scan.nextLine();
+                if (tileNum.toLowerCase().equals("reset")) {
+                        reset = true;
+                        break;
+                    }
+                startPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
+                startPosCol = Integer.parseInt(tileNum.substring(1))-1;
             }
             System.out.println("Where would you like the other end of your ship to be placed?: ");
             tileNum = scan.nextLine();
+
+            if (tileNum.toLowerCase().equals("reset")) {
+                    reset = true;
+                }
             int endPosRow = (int)(tileNum.toLowerCase().charAt(0)) - 'a';
             int endPosCol = Integer.parseInt(tileNum.substring(1))-1;
 
@@ -38,10 +61,10 @@ public class PlayerBoard {
             }
             // Second while loop checks to see if the end pos is a valid location on the grid. 
             // It also checks if the end pos is the correct distance away from the start pos
-            while ((Math.abs(startPosRow - endPosRow) + Math.abs(startPosCol - endPosCol) != ships[ship]-1)
+            while (((Math.abs(startPosRow - endPosRow) + Math.abs(startPosCol - endPosCol) != ships[ship]-1)
             || (startPosRow != endPosRow && startPosCol != endPosCol)
             || (endPosRow > 9 || endPosRow < 0 || endPosCol > 9 || endPosCol < 0)
-            || game[endPosRow][endPosCol] != 0 || overlapping == true) {
+            || game[endPosRow][endPosCol] != 0 || overlapping == true) && reset == false) {
                 System.out.println("Invalid placement! Try again: ");
                 tileNum = scan.nextLine();
                 // If the user types "reset", the program sends them back to the beginning of the ship placement
@@ -61,6 +84,11 @@ public class PlayerBoard {
                 }
             }
 
+            if (reset == true) {
+                ship -= 1;
+                continue;
+            }   
+
             // Actually adds the ship tiles to the grid once it's validated.
             for (int r = Math.min(startPosRow, endPosRow); r <= Math.max(startPosRow, endPosRow); r ++) {
                 for (int c = Math.min(startPosCol, endPosCol); c <= Math.max(startPosCol, endPosCol); c ++) {
@@ -70,8 +98,6 @@ public class PlayerBoard {
             printBoard();
         }
     }
-
-    public void
 
     public void speedSetup () {
         
